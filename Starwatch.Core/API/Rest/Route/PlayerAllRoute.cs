31938095edfc3a60ffb834a1711f51856d172637
Lachee <lachlan@hotmail.com>
2019-08-25 23:02:13 +1000

    Fixed authentication too low over gateway

--- Starwatch.Core/API/Rest/Route/PlayerAllRoute.cs
+++ Starwatch.Core/API/Rest/Route/PlayerAllRoute.cs
@@ -6,20 +6,27 @@ using Starwatch.API.Gateway;
 
 namespace Starwatch.API.Rest.Route
 {
-    [Route("/player/all", AuthLevel.SuperBot)]
+    [Route("/player/all", AuthLevel.Admin)]
     class PlayerAllRoute : RestRoute, IGatewayRoute
     {
-        public PlayerAllRoute(RestHandler handler, Authentication authentication) : base(handler, authentication) { }
+        public bool IsGateway { get; private set; }
+
+        public PlayerAllRoute(RestHandler handler, Authentication authentication) : base(handler, authentication) { IsGateway = false; }
 
         public string GetRouteName() => "/player/all";
+
         public void SetGateway(EventConnection gateway)
         {
             this.Handler = gateway.API.RestHandler;
             this.Authentication = gateway.Authentication;
+            this.IsGateway = true;
         }
 
         public override RestResponse OnGet(Query query)
         {
+            if (!IsGateway && AuthenticationLevel < AuthLevel.SuperBot)
+                return RestResponse.Forbidden;
+
             var players = Starbound.Connections.GetPlayersEnumerator();
             return new RestResponse(RestStatus.OK, res: players.ToArray());
         }
