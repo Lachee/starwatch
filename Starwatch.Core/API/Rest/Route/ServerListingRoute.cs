7ed03c6eef68713d9471f3444df19c541a928d42
Lachee <lachlan@hotmail.com>
2019-09-14 16:53:53 +1000

    Added a compound statistics for easier record fetching.

---
+++ Starwatch.Core/API/Rest/Route/ServerListingRoute.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json.Linq;
+using Starwatch.API.Gateway;
+using Starwatch.API.Gateway.Event;
+using Starwatch.API.Rest.Routing;
+using Starwatch.API.Web;
+
+namespace Starwatch.API.Rest.Route
+{
+    [Route("/server/statistics/listing", AuthLevel.Bot)]
+    class ServerListingRoute : RestRoute
+    {
+        public override bool Silent => true;
+
+        public ServerListingRoute(RestHandler handler, Authentication authentication) : base(handler, authentication) { }
+
+
+        public override RestResponse OnGet(Query query)
+        {
+            var stat = new CompoundStatistic();
+            stat.Statistics =  Handler.Starbound.GetStatisticsAsync().Result;
+            stat.Configurator = Handler.Starbound.Configurator;
+
+            return new RestResponse(RestStatus.OK, res: stat);
+        }
+
+        class CompoundStatistic
+        {
+            public Starbound.Statistics Statistics { get; set; }
+            public Starbound.Configurator Configurator { get; set; }
+        }
+
+
+    }
+}
