58e3b70c1f0917141c7c05ccfd82c834a3f99210
MiniOranges <user.674711@gmail.com>
2021-08-06 18:48:17 -0500

    Add unit test for message parsing and fix bug.

---
+++ Starwatch.Core.Tests/Entities/MessageTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Starwatch.Entities;
+using System.Diagnostics;
+
+namespace Starwatch.Core.Tests.Entities
+{
+    [TestClass]
+    public class MessageTest
+    {
+        [TestMethod]
+        public void TestLumiMessage ()
+        {
+            // This is an example of a group of messages sent which are confirmed
+            // to cause Starwatch to crash the server.
+
+            string[] lumiMessages = new string[]
+            {
+                "[Info] Chat: <^pink;Lumi^reset;> ^white;",
+                "return math.factor(749)",
+                "--------",
+                "7 * 107"
+            };
+
+            foreach (string msg in lumiMessages)
+            {
+                Message m = Message.Parse(null, msg);
+                Console.WriteLine(
+                    $"Message Original: \"{msg}\"\n" +
+                    $"-- .Content       \"{m.Content}\"\n" +
+                    $"-- .Author        \"{m.Author}\"\n" +
+                    $"-- .IsChat        \"{m.IsChat}\"\n" +
+                    $"-- .Level         \"{m.Level}\""
+                );
+            }
+        }
+    }
+}
