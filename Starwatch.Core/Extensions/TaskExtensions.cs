b115e7f26ce1d64f33a914549bd191bfd59244d3
- <56451580+MiniOranges@users.noreply.github.com>
2022-05-01 12:32:19 -0500

    Add CallAsyncWithLog extension to Tasks
    Asynchronous call-and-forget in synchronous methods. Logs when an exception is hit just for general safety.

---
+++ Starwatch.Core/Extensions/TaskExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starwatch.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static Task CallAsyncWithLog (this Task task, Logging.Logger logger, string format = null)
+        {
+            return task.ContinueWith((t) =>
+            {
+                if (!(t.Exception is null))
+                {
+                    if (format is null)
+                        logger.LogError(t.Exception);
+
+                    else
+                        logger.LogError(t.Exception, format);
+
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
